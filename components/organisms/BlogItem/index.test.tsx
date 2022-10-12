a6e00fa91659157bca87b6483dfb071ff34fca9f
JeongHean Kim <dev.yakuza@gmail.com>
2022-10-12 21:33:49 +0900

    test: Refactor test code for checking component order (#236)

--- components/organisms/BlogItem/index.test.tsx
+++ components/organisms/BlogItem/index.test.tsx
@@ -13,14 +13,15 @@ describe('<BlogItem />', () => {
       />,
     )
 
-    expect(mockGrid.mock.calls[0][0].item).toBe(true)
-    expect(mockGrid.mock.calls[0][0].xs).toBe(12)
+    const grid = mockGrid.mock.calls[0][0]
+    expect(grid.item).toBe(true)
+    expect(grid.xs).toBe(12)
 
-    const titleComponent = mockGrid.mock.calls[0][0].children[0]
+    const titleComponent = grid.children[0]
     expect(titleComponent.type.name).toBe('Typography')
     expect(titleComponent.props.children).toBe('This is the blog title.')
 
-    const contentsComponent = mockGrid.mock.calls[0][0].children[1]
+    const contentsComponent = grid.children[1]
     expect(contentsComponent.type.name).toBe('Typography')
     expect(contentsComponent.props.children).toBe('This is the blog contents.')
 
