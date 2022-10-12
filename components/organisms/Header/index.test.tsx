24bf9a195aaee0a7b39365ab47fed6f8752cbc1b
dev-yakuza <dev.yakuza@gmail.com>
2022-10-12 21:29:01 +0900

    test: Refactor test code for checking component order

--- components/organisms/Header/index.test.tsx
+++ components/organisms/Header/index.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 
-import { mockAppBar, mockToolbar, mockTypography } from 'utils/test/mocks'
+import { mockAppBar } from 'utils/test/mocks'
 
 import { Header } from '.'
 
@@ -8,19 +8,24 @@ describe('<Header />', () => {
   it('Rendered well', async () => {
     const { container } = render(<Header />)
 
-    expect(mockAppBar.mock.calls[0][0].position).toBe('static')
-    expect(mockAppBar.mock.calls[0][0].elevation).toBe(0)
-    expect(mockAppBar.mock.calls[0][0].style).toEqual({
+    const appBar = mockAppBar.mock.calls[0][0]
+    expect(appBar.position).toBe('static')
+    expect(appBar.elevation).toBe(0)
+    expect(appBar.style).toEqual({
       backgroundColor: '#FFF',
     })
 
-    expect(mockToolbar.mock.calls[0][0].sx).toEqual({ flexWrap: 'wrap' })
+    const toolbar = appBar.children
+    expect(toolbar.type.name).toBe('Toolbar')
+    expect(toolbar.props.sx).toEqual({ flexWrap: 'wrap' })
 
-    expect(mockTypography.mock.calls[0][0].variant).toBe('h6')
-    expect(mockTypography.mock.calls[0][0].color).toBe('inherit')
-    expect(mockTypography.mock.calls[0][0].noWrap).toBe(true)
-    expect(mockTypography.mock.calls[0][0].sx).toEqual({ flexGrow: 1 })
-    expect(mockTypography.mock.calls[0][0].children).toBe('Blog App')
+    const appTitle = toolbar.props.children
+    expect(appTitle.type.name).toBe('Typography')
+    expect(appTitle.props.variant).toBe('h6')
+    expect(appTitle.props.color).toBe('inherit')
+    expect(appTitle.props.noWrap).toBe(true)
+    expect(appTitle.props.sx).toEqual({ flexGrow: 1 })
+    expect(appTitle.props.children).toBe('Blog App')
 
     expect(container).toMatchSnapshot()
   })
