85d44e8e61ee6a4a12b6a9c7a96e8a2aeebf854c
dev-yakuza <dev.yakuza@gmail.com>
2022-10-17 21:49:04 +0900

    test: Add Emotion serializer for checking CSS

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,11 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+
+import { matchers, createSerializer } from '@emotion/jest'
+expect.extend(matchers)
+expect.addSnapshotSerializer(createSerializer())
+
+module.exports = {
+  snapshotSerializers: ['@emotion/jest/serializer'],
+}
