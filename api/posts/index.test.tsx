6ff81095f2ab5d2f59eee60539545520c41a5842
dev-yakuza <dev.yakuza@gmail.com>
2022-11-01 21:28:03 +0900

    feat: Add post API

--- api/posts/index.test.tsx
+++ api/posts/index.test.tsx
@@ -8,14 +8,15 @@ import MockAdapter from 'axios-mock-adapter'
 import type { Post } from 'types'
 import { queryClient } from 'utils/test'
 
-import { useGetPosts } from './index'
-import mockPosts from './mockData/posts.json'
+import { useGetPosts, useGetPost } from './index'
+import mockPostData from './mockData/post.json'
+import mockPostsData from './mockData/posts.json'
 
 const mockAxios = new MockAdapter(axios)
 
 describe('[API] useGetPosts', () => {
   it('success', async () => {
-    mockAxios.onGet('/posts').reply(200, mockPosts)
+    mockAxios.onGet('/posts').reply(200, mockPostsData)
     const wrapper = ({ children }: { children: JSX.Element }) => (
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     )
@@ -35,7 +36,7 @@ describe('[API] useGetPosts', () => {
       expect(result?.current.isSuccess).toBe(true)
     })
 
-    expect(result?.current.data).toEqual(mockPosts)
+    expect(result?.current.data).toEqual(mockPostsData)
   })
 
   it('fail', async () => {
@@ -51,6 +52,56 @@ describe('[API] useGetPosts', () => {
 
     await act(async () => {
       result = renderHook(() => useGetPosts(), {
+        wrapper,
+      }).result
+    })
+
+    await waitFor(() => {
+      expect(result?.current.isError).toBe(true)
+    })
+
+    expect((result?.current.error as AxiosError).response?.status).toBe(400)
+  })
+})
+
+describe('[API] useGetPost', () => {
+  it('success', async () => {
+    mockAxios.onGet('/posts/1').reply(200, mockPostData)
+    const wrapper = ({ children }: { children: JSX.Element }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+    let result:
+      | {
+          current: UseQueryResult<ReadonlyArray<Post>, unknown>
+        }
+      | undefined
+
+    await act(async () => {
+      result = renderHook(() => useGetPost({ id: 1 }), {
+        wrapper,
+      }).result
+    })
+
+    await waitFor(() => {
+      expect(result?.current.isSuccess).toBe(true)
+    })
+
+    expect(result?.current.data).toEqual(mockPostData)
+  })
+
+  it('fail', async () => {
+    mockAxios.onGet('/posts/1').reply(400)
+    const wrapper = ({ children }: { children: JSX.Element }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+    let result:
+      | {
+          current: UseQueryResult<ReadonlyArray<Post>, unknown>
+        }
+      | undefined
+
+    await act(async () => {
+      result = renderHook(() => useGetPost({ id: 1 }), {
         wrapper,
       }).result
     })
