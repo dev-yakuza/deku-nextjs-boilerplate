7a1df3e238f518a61e5f489e97e83bdda27c5c73
JeongHean Kim <dev.yakuza@gmail.com>
2022-11-23 15:50:51 +0900

    feat: Add post detail page (#273)

---
+++ test/posts/[id].test.tsx
@@ -0,0 +1,75 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+
+import mockPostData from 'api/posts/mockData/post.json'
+import type { Post } from 'types'
+import {
+  queryClient,
+  mockBlogDetail,
+  useRouter,
+  getMockRouter,
+} from 'utils/test'
+
+import PostDetail from '../../pages/posts/[id]'
+
+let mockResponse: { readonly data: Post | undefined } = {
+  data: undefined,
+}
+jest.mock('api/posts', () => ({ useGetPost: () => mockResponse }))
+
+describe('PostDetail', () => {
+  beforeEach(() => {
+    const mockRouter = getMockRouter('/posts/1')
+    mockRouter.query = {
+      id: '1',
+    }
+    useRouter.mockImplementation(() => mockRouter)
+  })
+
+  it('rendered well with data', () => {
+    mockResponse = { data: mockPostData }
+
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <PostDetail />
+      </QueryClientProvider>,
+    )
+
+    expect(mockBlogDetail.mock.calls.length).toBe(1)
+    expect(mockBlogDetail.mock.calls[0][0].post).toBe(mockPostData)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('rendered well without data', () => {
+    mockResponse = { data: undefined }
+
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <PostDetail />
+      </QueryClientProvider>,
+    )
+
+    expect(mockBlogDetail.mock.calls.length).toBe(1)
+    expect(mockBlogDetail.mock.calls[0][0].post).toBe(undefined)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('rendered well with wrong ID query', () => {
+    const mockRouter = getMockRouter('/posts/aaa')
+    mockRouter.query = {
+      id: 'aaa',
+    }
+    useRouter.mockImplementation(() => mockRouter)
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <PostDetail />
+      </QueryClientProvider>,
+    )
+
+    expect(mockBlogDetail.mock.calls.length).toBe(1)
+    expect(mockBlogDetail.mock.calls[0][0].post).toBe(undefined)
+  })
+})
--- utils/test/mocks/components/templates.tsx
+++ utils/test/mocks/components/templates.tsx
@@ -1,19 +1,28 @@
 const mockBlogList = jest.fn()
+const mockBlogDetail = jest.fn()
 
 jest.mock('components/templates', () => {
-  const { BlogList: BlogListComponent, ...rest } = jest.requireActual(
-    'components/templates',
-  )
+  const {
+    BlogList: BlogListComponent,
+    BlogDetail: BlogDetailComponent,
+    ...rest
+  } = jest.requireActual('components/templates')
 
   const BlogList = (props: typeof BlogListComponent) => {
     mockBlogList(props)
     return <BlogListComponent {...props} />
   }
 
+  const BlogDetail = (props: typeof BlogDetailComponent) => {
+    mockBlogDetail(props)
+    return <BlogDetailComponent {...props} />
+  }
+
   return {
     BlogList,
+    BlogDetail,
     ...rest,
   }
 })
 
-export { mockBlogList }
+export { mockBlogList, mockBlogDetail }
