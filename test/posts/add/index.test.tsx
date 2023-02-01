1cac399fb456be8af869a933fb5db5296f778dd3
JeongHean Kim <dev.yakuza@gmail.com>
2023-02-01 20:23:11 +0900

    feat: Add <Add /> page component (#311)

---
+++ test/posts/add/index.test.tsx
@@ -0,0 +1,132 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { AxiosError } from 'axios'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+
+import type { CreatePostParams } from 'api/posts'
+import type { Post } from 'types'
+import { queryClient, mockCreateBlogPost, getMockRouter } from 'utils/test'
+
+import CreateBlogPost from '../../../pages/posts/add'
+
+let mockAPISuccess = true
+let mockIsLoading = false
+let mockIsCalledOnSuccess = false
+let mockIsCalledOnError = false
+jest.mock('api/posts', () => ({
+  useCreatePost: ({
+    onSuccess,
+    onError,
+  }: {
+    readonly onSuccess?: (data: Post) => void
+    readonly onError?: (error: AxiosError) => void
+  }) => ({
+    mutate: ({ userId, title, description }: CreatePostParams) => {
+      if (mockAPISuccess) {
+        if (onSuccess != null) {
+          mockIsCalledOnSuccess = true
+          onSuccess({
+            userId,
+            id: 1,
+            title,
+            body: description,
+          })
+        }
+      } else {
+        if (onError != null) {
+          mockIsCalledOnError = true
+          onError(new AxiosError('Error'))
+        }
+      }
+    },
+    isLoading: mockIsLoading,
+  }),
+}))
+
+describe('<CreateBlogPost />', () => {
+  beforeEach(() => {
+    mockAPISuccess = true
+    mockIsLoading = false
+    mockIsCalledOnSuccess = false
+    mockIsCalledOnError = false
+  })
+
+  it('rendered well', () => {
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <CreateBlogPost />
+      </QueryClientProvider>,
+    )
+
+    expect(mockCreateBlogPost.mock.calls.length).toBe(1)
+    expect(mockCreateBlogPost.mock.calls[0][0].isCreating).toBe(false)
+    expect(mockCreateBlogPost.mock.calls[0][0].onCreatePost.name).toBe(
+      'handleCreate',
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('rendered well with isLoading', () => {
+    mockIsLoading = true
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CreateBlogPost />
+      </QueryClientProvider>,
+    )
+
+    expect(mockCreateBlogPost.mock.calls[0][0].isCreating).toBe(true)
+  })
+
+  it('success to create post', () => {
+    mockAPISuccess = true
+    const routerPush = jest.fn()
+    const mockRouter = getMockRouter()
+    mockRouter.push = routerPush
+
+    render(
+      <RouterContext.Provider value={mockRouter}>
+        <QueryClientProvider client={queryClient}>
+          <CreateBlogPost />
+        </QueryClientProvider>
+      </RouterContext.Provider>,
+    )
+
+    expect(mockIsCalledOnSuccess).toBe(false)
+    expect(mockIsCalledOnError).toBe(false)
+    expect(routerPush.mock.calls.length).toBe(0)
+
+    // Call creating post
+    mockCreateBlogPost.mock.calls[0][0].onCreatePost(
+      'test title',
+      'test description',
+    )
+
+    expect(mockIsCalledOnSuccess).toBe(true)
+    expect(mockIsCalledOnError).toBe(false)
+    expect(routerPush.mock.calls[0][0]).toBe('/')
+  })
+
+  it('fail to create post', () => {
+    mockAPISuccess = false
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CreateBlogPost />
+      </QueryClientProvider>,
+    )
+
+    expect(mockIsCalledOnSuccess).toBe(false)
+    expect(mockIsCalledOnError).toBe(false)
+
+    // Call creating post
+    mockCreateBlogPost.mock.calls[0][0].onCreatePost(
+      'test title',
+      'test description',
+    )
+
+    expect(mockIsCalledOnSuccess).toBe(false)
+    expect(mockIsCalledOnError).toBe(true)
+  })
+})
--- utils/test/mocks/components/templates.tsx
+++ utils/test/mocks/components/templates.tsx
@@ -1,10 +1,12 @@
 const mockBlogList = jest.fn()
 const mockBlogDetail = jest.fn()
+const mockCreateBlogPost = jest.fn()
 
 jest.mock('components/templates', () => {
   const {
     BlogList: BlogListComponent,
     BlogDetail: BlogDetailComponent,
+    CreateBlogPost: CreateBlogPostComponent,
     ...rest
   } = jest.requireActual('components/templates')
 
@@ -18,11 +20,17 @@ jest.mock('components/templates', () => {
     return <BlogDetailComponent {...props} />
   }
 
+  const CreateBlogPost = (props: typeof CreateBlogPostComponent) => {
+    mockCreateBlogPost(props)
+    return <CreateBlogPostComponent {...props} />
+  }
+
   return {
     BlogList,
     BlogDetail,
+    CreateBlogPost,
     ...rest,
   }
 })
 
-export { mockBlogList, mockBlogDetail }
+export { mockBlogList, mockBlogDetail, mockCreateBlogPost }
